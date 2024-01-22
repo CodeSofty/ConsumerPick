963818747b8544de3785d92f08d50b67e602e516
CodeSoftly <eclittlejohn@yahoo.com>
2024-01-22 13:11:22 -0600

    Added service to app to get products

---
+++ ConsumerPick/Services/JsonFileProductService.cs
@@ -0,0 +1,34 @@
+using System.Text.Json;
+using ConsumerPick.Models;
+
+namespace ConsumerPick.Services
+{
+
+    public class JsonFileProductService
+    {
+        public JsonFileProductService(IWebHostEnvironment webHostEnvironment)
+        {
+            WebHostEnvironment = webHostEnvironment;
+        }
+
+        public IWebHostEnvironment WebHostEnvironment { get; }
+
+        private string JsonFileName
+        {
+            get { return Path.Combine(WebHostEnvironment.WebRootPath, "data", "products.json"); }
+        }
+
+        public IEnumerable<Product> GetProducts()
+        {
+            using (var jsonFileReader = File.OpenText(JsonFileName))
+            {
+                return JsonSerializer.Deserialize<Product[]>(jsonFileReader.ReadToEnd(),
+                    new JsonSerializerOptions
+                    {
+                        PropertyNameCaseInsensitive = true
+                    });
+            }
+        }
+    }
+    
+}
